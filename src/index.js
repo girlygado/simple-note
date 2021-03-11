ad258ad8450805919cc88ec15b56d246bfdd49d6
Gil <girlyg@goetu.com>
2021-03-11 15:33:09 +0800

    update import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './App'
 import './app.css'
 
 import { GlobalProvider } from './context/GlobalContext'
